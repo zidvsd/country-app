043e3611a863a9a3522088b4317ffbe61ebfb526
Zid Visda <rashidvisda@gmail.com>
2025-04-14 21:17:53 +0800

    Polished dark mode values

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -2,39 +2,42 @@ import React, { useState } from "react";
 
 const Filter = () => {
   const [isOpen, setIsOpen] = useState(false);
-
+  const [selectedRegion, setSelectedRegion] = useState("Filter by Region");
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
-
+  const handleRegionSelect = (region) => {
+    setSelectedRegion(region);
+    setIsOpen(false);
+    if (region === "All") {
+      setSelectedRegion("Filter by Region");
+    } else {
+      selectedRegion(region);
+    }
+  };
+  const countries = ["All", "Africa", "America", "Asia", "Europe", "Oceania"];
   return (
-    <div className="inline-block text-left">
+    <div className="inline-block text-left font-semibold">
       <button
         onClick={toggleDropdown}
-        className="px-4 py-2 bg-white text-black rounded-md focus:outline-none"
+        className="cursor-pointer px-4 py-2 bg-white text-black rounded-md focus:outline-none shadow-md w-auto dark:bg-dark-blue dark:text-white"
       >
-        Filter by Region
+        {selectedRegion}
       </button>
 
       {isOpen && (
-        <div className="w-40 mt-2 bg-white shadow-md rounded-md border border-gray-200">
-          <ul>
-            <li>
-              <a
-                href="#"
-                className="block px-4 py-2 text-gray-700 hover:bg-gray-200"
-              >
-                Item 1
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block px-4 py-2 text-gray-700 hover:bg-gray-200"
-              >
-                Item 2
-              </a>
-            </li>
+        <div className="w-auto mt-2  dark:text-white  bg-dark-blue shadow-md   ">
+          <ul className="rounded-md">
+            {countries.map((region, index) => (
+              <li key={index}>
+                <a
+                  onClick={() => handleRegionSelect(region)}
+                  className="block px-4 py-2 bg-white dark:bg-dark-blue dark:text-white hover:bg-neutral-500 cursor-pointer"
+                >
+                  {region}
+                </a>
+              </li>
+            ))}
           </ul>
         </div>
       )}
