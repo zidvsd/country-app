74552b59e313f498929aff4592d7775e6b521b8c
Zid Visda <rashidvisda@gmail.com>
2025-04-14 13:27:42 +0800

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="container mx-auto  w-full">
+      <h1 className="text-md font-nunito-sans max-w-small">hi</h1>
+    </div>
+  );
+};
+
+export default App;
